15f38d662dd17a9b454b8cef2254758a3a923567
asiancart <asiancart@hotmail.com>
2024-12-29 16:30:21 +0300

    first commit

---
+++ MyWebApp/Models/Dish.cs
@@ -0,0 +1,13 @@
+namespace RestaurantList.Models
+{
+    public class Dish 
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public double Price { get; set; }
+
+        public List<RestaurantDish>? RestaurantDishes { get; set; }
+    }
+}
